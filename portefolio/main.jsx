3cf936fb7fc4afb64a68d19bd2494619083fce92
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-07-30 19:32:40 +0100

    redirect added

--- portefolio/main.jsx
+++ portefolio/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './app.css';
-import Layout from './components/AppLayout';
-import About from './pages/About';
-import Home from './pages/Home';
-import Works from './pages/Works';
-import NotFound from './pages/NotFound';
+import Layout from './src/components/AppLayout';
+import About from './src/pages/About';
+import Home from './src/pages/Home';
+import Works from './src/pages/Works';
+import NotFound from './src/pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
