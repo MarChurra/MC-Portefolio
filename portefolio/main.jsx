2937549cf6c282ae9d822da535402d99b6c9971f
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-07-30 19:14:01 +0100

    Update main.jsx

--- portefolio/main.jsx
+++ portefolio/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '/app.css';
-import Layout from '/components/AppLayout.jsx';
-import About from '/pages/About.jsx'
-import Home from '/pages/Home.jsx'
-import Works from '/pages/Works.jsx'
-import NotFound from '/pages/NotFound.jsx';
+import Layout from '../components/AppLayout.jsx';
+import About from '../pages/About.jsx'
+import Home from '../pages/Home.jsx'
+import Works from '../pages/Works.jsx'
+import NotFound from '/..pages/NotFound.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
