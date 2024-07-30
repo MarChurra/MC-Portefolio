5951530c07e16380b63922ef6340239921fc0036
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-07-30 23:14:04 +0100

    css updates and fixing the logic in the arrow button to switch pages

--- portefolio/src/components/AppLayout.jsx
+++ portefolio/src/components/AppLayout.jsx
@@ -2,73 +2,71 @@ import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import Media from './Media.jsx'
-import Navigation from './Navigation.jsx'
-import Footer from './Footer.jsx'
-import AnimatedIcon from '../components/AnimatedIcon'
+import Media from './Media.jsx';
+import Navigation from './Navigation.jsx';
+import Footer from './Footer.jsx';
+import AnimatedIcon from '../components/AnimatedIcon';
 
-
-//Logic for the page navigation
+// Logic for the page navigation
 function Layout() {
-  const [currentPage, setCurrentPage] = useState('Works')
+
   const navigate = useNavigate();
   const location = useLocation();
-
+  const [currentPage, setCurrentPage] = useState('');
 
   const handleClick = () => {
-    if (currentPage === 'Home') {
+    if (location.pathname === '/') {
       setCurrentPage('About');
-    } else if (currentPage === 'About') {
+    } else if (location.pathname === '/about') {
       setCurrentPage('Works');
     }
-  }
-
-  useEffect(() => {
-    if (currentPage === 'Home') {
-      navigate('/');
-    } else if (currentPage === 'About') {
-      navigate('/about');
-    } else if (currentPage === 'Works') {
-      navigate('/works');
-    }
-  }, [currentPage, navigate]);
-
-  useEffect(() => {
-    localStorage.setItem('currentUrl', location.pathname);
-  }, [location.pathname]);
-
-  useEffect(() => {
-    const storedURL = localStorage.getItem('currentUrl')
-    if (storedURL) {
-      navigate(storedURL)
-    } else {
-      return null
-    }
-  }, [navigate])
+  };
 
   const handleNavigationClick = (page) => {
     setCurrentPage(page);
   };
 
+  // Navigate when currentPage changes
+  useEffect(() => {
+    switch (currentPage) {
+      case 'Home':
+        navigate('/');
+        break;
+      case 'About':
+        navigate('/about');
+        break;
+      case 'Works':
+        navigate('/works');
+        break;
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    localStorage.setItem('currentUrl', location.pathname);
+  }, [location.pathname]);
+
+  // Update the current page based on the URL when the component mounts
+  useEffect(() => {
+    const storedURL = localStorage.getItem('currentUrl');
+    if (storedURL) {
+      setCurrentPage(storedURL);
+    } else {
+      setCurrentPage('/');
+    }
+  }, [currentPage]);
+
 
   // App Layout
   return (
     <div className="content">
-
       <Navigation onNavClick={handleNavigationClick} />
-
-      <div className='main-content'>
+      <div className="main-content">
         <Outlet />
         <Media />
       </div>
-
-      {currentPage === 'Works' ? null : <AnimatedIcon
-        handleClick={handleClick}
-      />}
-
+      {currentPage && currentPage !== '/works' && <AnimatedIcon handleClick={handleClick} />}
       <Footer />
-
-    </div >
+    </div>
   );
 }
 
