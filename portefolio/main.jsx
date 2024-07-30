d649a1218642a2f4f517da1d3196ebb2f0377e99
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-07-30 19:35:20 +0100

    Update main.jsx

--- portefolio/main.jsx
+++ portefolio/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './app.css';
-import Layout from './src/components/AppLayout';
-import About from './src/pages/About';
-import Home from './src/pages/Home';
-import Works from './src/pages/Works';
-import NotFound from './src/pages/NotFound';
+import '/app.css';
+import Layout from '/src/components/AppLayout';
+import About from '/src/pages/About';
+import Home from '/src/pages/Home';
+import Works from '/src/pages/Works';
+import NotFound from '/src/pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
